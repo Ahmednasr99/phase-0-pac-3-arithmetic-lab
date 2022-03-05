995744a3f9315618151a4b34d7bb9f9ee0398400
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-03-05 03:56:06 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(x,y){
+    return x+y
+}
+
+function subtract(x,y){
+    return x-y
+}
+
+function multiply(x,y){
+    return x*y
+}
+
+function divide(x,y){
+    return x/y
+}
+
+function increment(n){
+    return console.log(n++) ;
+}
+increment(100);
+function decrement(n){
+    
+    return n--;
+}
+decrement(10);
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+function preserveDecimal(n){
+    return parseInt(n);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
